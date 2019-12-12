a2c21f9224ed7656242ca81eb98efb2c3f14cc9b
Kgaugelo <kgaugelo.ramaramela@umuzi.org>
2019-12-12 12:56:09 +0200

    Person Full Project

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainProgram {
     public static void main(String[] args) {
         Person person = new Person("Ryan", 30, "male", new String[]{"being a hardarse", "agile", "ssd hard drives"});
         System.out.println(person.hello());
